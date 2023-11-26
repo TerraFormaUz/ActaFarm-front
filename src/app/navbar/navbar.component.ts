b1a32b23470a1f8682b603e8827a5739f3672ab4
umid0608 <umidazamqulov88@gmail.com>
2023-11-26 08:01:01 +0500

    fornt-commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "navbar-component, NavbarComponent",
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+    ngOnInit(): void {
+    }
+    constructor(private router: Router) { }
+    
+  goToResourseList() {
+    this.router.navigateByUrl('/resource-list');
+  }
+}
---
+++ src/app/resource/resource.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-resource',
+  templateUrl: './resource.component.html',
+  styleUrls: ['./resource.component.css']
+})
+export class ResourceComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+}
